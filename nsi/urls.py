677c16c007cc48c02efe0ea083d56ea64ae1ecc7
Derrick Jackson <derrick.jackson73@gmail.com>
2016-03-06 10:50:33 -0500

    added 404 and 500 pages.

--- nsi/urls.py
+++ nsi/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     url(r'^', include('internal.urls', namespace='internal')),
@@ -8,4 +10,7 @@ urlpatterns = [
 
     # Shared Application
     url(r'^shared/', include('shared.urls', namespace='shared')),
+
+    url(r'^404/$', TemplateView.as_view(template_name='404.html',)),
+    url(r'^500/$', TemplateView.as_view(template_name='500.html',)),
 ]
